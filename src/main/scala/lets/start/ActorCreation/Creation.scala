f45a00598ba6d28257c139b1a0f4e4b8ea052173
shivangi Gupta <shivangi.gupta@knoldus.in>
2019-09-05 20:09:55 +0530

    Added 'Creating an Actor with Props' Example

---
+++ src/main/scala/lets/start/ActorCreation/Creation.scala
@@ -0,0 +1,55 @@
+package lets.start.ActorCreation
+
+import akka.actor.{Actor, ActorSystem, Props}
+import lets.start.ActorCreation.MusicController.{Play, Stop}
+import lets.start.ActorCreation.MusicPlayer.{StartMusic, StopMusic}
+
+//Music controller message
+object MusicController {
+  sealed trait ControllerMessage
+  case object Play extends ControllerMessage
+  case object Stop extends ControllerMessage
+
+  val props = Props[MusicController]
+}
+
+//Music Controller Actor
+class MusicController extends Actor{
+  def receive: Receive = {
+    case Play => println("Music started...")
+    case Stop => println("Music stopped...")
+  }
+}
+
+//Music Player Mesaages
+object MusicPlayer {
+  sealed trait PlayMessage
+  case object StopMusic extends PlayMessage
+  case object StartMusic extends PlayMessage
+}
+
+//Music Player Actor
+class MusicPlayer extends Actor {
+  def receive: Receive = {
+    case StopMusic => println("I don't want to stop music")
+    case StartMusic =>
+      val controller = context.actorOf(MusicController.props, "controller")
+      controller ! Play
+    case _ => println("Unknown message")
+  }
+}
+
+object Creation extends App {
+
+  //Create creation actor system
+  val system = ActorSystem("creation")
+
+  //Create MusicPlayer actor
+  val player = system.actorOf(Props[MusicPlayer], "player")
+
+  //Send StartMusic message to the actor
+  player ! StartMusic
+
+  //Send StopMusic message to the actor
+  player ! StopMusic
+}
